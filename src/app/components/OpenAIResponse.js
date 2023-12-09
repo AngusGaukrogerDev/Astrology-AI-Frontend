ab143d6e7c168ecae512248eb83f702af47d3aa2
AngusGaukrogerDev <agaukroger77@gmail.com>
2023-12-09 18:04:08 +0000

    basicpullfrombackend

---
+++ src/app/components/OpenAIResponse.js
@@ -0,0 +1,28 @@
+'use client'
+import axios from 'axios';
+import GetDataButton from './GetDataButton';
+import { useState } from 'react';
+
+const OpenAIResponse = () => {
+    const [result, setResult] = useState(null);
+    const handleGetReading = async () => {
+        try {
+          const response = await fetch('http://localhost:3000/generateCompletion');
+          const data = await response.json()
+          console.log(data);
+          setResult(data.completion.message.content);
+        } catch (error) {
+          console.error('Error getting reading:', error);
+        }
+      };
+        return(
+            <div className="w-full h-screen flex flex-col justify-center items-center">
+                <button onClick={handleGetReading}>Get Reading</button>
+
+                <h2>
+                    {result}
+                </h2>
+            </div>
+        );
+}
+export default OpenAIResponse;
